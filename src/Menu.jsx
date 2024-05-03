481342794de6f748fcaaacc0cbecb4180d69cc22
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2024-05-03 11:22:49 +0530

    Routing

---
+++ src/Menu.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <div>
+       <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {/* <Link to="/home"></Link> */}
+            {/* <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/contact">Contact Us</Nav.Link> */}
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default Menu
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,14 @@ import Classcomponent from './Classcomponent.jsx'
 import Task1 from './Task1.jsx'
 import Task1Class from './Task1Class.jsx'
 import ObjectAccess from './ObjectAccess.jsx'
+import BootstrapComponent from './BootstrapComponent.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter,Routes,Route, HashRouter } from 'react-router-dom'
+import Menu from './Menu.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +23,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Classcomponent color="red"/> */}
     {/* <Task1 /> */}
     {/* <Task1Class /> */}
-    <ObjectAccess />
+    {/* <ObjectAccess /> */}
+    {/* <BootstrapComponent /> */}
+    <HashRouter>
+    <Menu />
+    <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+    </Routes>
+    </HashRouter>
+    
   </React.StrictMode>,
 )
